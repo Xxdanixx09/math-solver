44225efaa3fb059d8f7b154a8a56f711e0d7e63f
Danielssm0 <desarrollo@trasferimos.com>
2026-09-02 21:17:22 -0500

    Latest

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def resolver_problema():
         """
 
         response = client.models.generate_content(
-            model='gemini-2.5-flash',
+            model='gemini-3.6-flash',
             contents=[prompt, image]
         )
         
